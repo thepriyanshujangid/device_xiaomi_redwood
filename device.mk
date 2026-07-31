b363590278136132a188b52c2b8bb33af10d76c7
Giovanni Ricca <giovanniricca@proton.me>
2026-07-31 19:18:42 +0530

    redwood: Convert overlays to RROs

--- device.mk
+++ device.mk
@@ -24,18 +24,19 @@ PRODUCT_COPY_FILES += \
 
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-# Overlays-RRO
 PRODUCT_PACKAGES += \
-    redwoodCNSettingsProviderOverlay \
-    redwoodCNWifiOverlay \
-    redwoodGLSettingsProviderOverlay \
-    redwoodGLWifiOverlay \
-    redwoodINSettingsProviderOverlay \
-    redwoodINWifiOverlay
+    ApertureOverlayRedwood
+
+PRODUCT_PACKAGES += \
+    FrameworkOverlayRedwood \
+    SettingsOverlayRedwood \
+    SettingsProviderOverlayRedwoodCN \
+    SettingsProviderOverlayRedwoodGL \
+    SettingsProviderOverlayRedwoodIN \
+    SystemUIOverlayRedwood \
+    WifiOverlayRedwoodCN \
+    WifiOverlayRedwoodGL \
+    WifiOverlayRedwoodIN
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
